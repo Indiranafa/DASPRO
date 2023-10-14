4f1cbbbbd26563fd8027db19c2603eac191fefd9
Indira Nafa <indiranafa24@gmail.com>
2023-10-15 02:34:17 +0700

    modif average

--- Jobsheet7/ForKelipatan14.java
+++ Jobsheet7/ForKelipatan14.java
@@ -20,7 +20,12 @@ public class ForKelipatan14 {
             }
         }
 
+        if (counter > 0) {
+            average = (double) jumlah / counter;
+        }
+
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, average);
     }
 }
