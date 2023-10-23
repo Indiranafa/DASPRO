a7bf2f4d722c2fd4b6028d6598c42f2fc34454d2
Indira Nafa <indiranafa24@gmail.com>
2023-10-23 10:51:16 +0700

    Coba Array Nilai

--- Jobsheet8/ArrayNilai14.java
+++ Jobsheet8/ArrayNilai14.java
@@ -9,7 +9,7 @@ public class ArrayNilai14 {
         int[] nilaiAkhir = new int[10];
 
         for (int i=0; i < 10; i++){
-            System.out.print("Masukkan nilai akhir ke- "+i+" : ");
+            System.out.print("Masukkan nilai akhir ke- " +i+ " : ");
             nilaiAkhir[i] = sc.nextInt();
         }
 
