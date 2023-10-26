2157450f20ed2d21be5f03f8992f7ac1d8f5a105
Indira Nafa <indiranafa24@gmail.com>
2023-10-26 08:50:01 +0700

    Modifikasi mengurutkan angka terbesar ke terkecil

--- Jobsheet8/BubbleSortExample14.java
+++ Jobsheet8/BubbleSortExample14.java
@@ -9,13 +9,13 @@ public class BubbleSortExample14 {
         int[] intData = {34, 18, 87, 72, 32, 54, 43};
         int temp = 0;
 
-        for (int i=0; i < intData.length; i++){
-            for(int j=1; j < intData.length-1; j++){
-                if (intData[j-1] > intData[j]) {
+        for (int i = 0; i < intData.length; i++){
+            for(int j = 0; j < intData.length-1; j++){
+                if (intData[j] < intData[j + 1]) {
                     //swap elemen
                     temp = intData[j];
-                    intData[j] = intData[j];
-                    intData[j-1] = temp;
+                    intData[j] = intData[j + 1];
+                    intData[j + 1] = temp;
                 }
             }
         }
